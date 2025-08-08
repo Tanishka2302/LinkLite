a8f90f6346b5c16e38968fd10613393ebf0f27b8
Tanishka Pandharpatte <tanishkapandharpatte2302@gmail.com>
2025-08-08 09:31:15 +0530

    Update Header.js

--- frontend/src/utils/components/layout/Header.js
+++ frontend/src/utils/components/layout/Header.js
@@ -26,12 +26,7 @@ const Header = () => {
             >
               Feed
             </Link>
-          /*  <Link 
-              to={`/profile/${user.id}`} 
-              className="px-3 py-2 text-gray-700 hover:text-blue-600 transition-colors"
-            >
-              Profile
-            </Link>*/
+         
             <button
               onClick={handleLogout}
               className="px-3 py-2 text-gray-700 hover:text-red-600 transition-colors"
