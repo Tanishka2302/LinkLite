80c3ea6ae4a38a328ee565cc724dcb4271e64495
Tanishka Pandarpatte <tanishkapandarpatte2302@gmail.com>
2025-09-21 17:22:12 +0530

    Fix: profile tab is added!

--- frontend/src/utils/components/layout/Header.js
+++ frontend/src/utils/components/layout/Header.js
@@ -28,12 +28,12 @@ const Header = () => {
             </Link>
 
             {
-            <Link 
-              to={`/profile/${user.id}`} 
-              className="px-3 py-2 text-gray-700 hover:text-blue-600 transition-colors"
-            >
-              Profile
-            </Link>
+         <Link 
+         to={`/profile/${user.id}`} 
+         className="px-3 py-2 text-gray-700 hover:text-blue-600 transition-colors"
+       >
+         Profile
+       </Link>
             }
 
             <button
