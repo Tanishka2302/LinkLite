722bc2b7603512b0b4be59617fa24cbd338d20a1
Tanishka Pandarpatte <tanishkapandarpatte2302@gmail.com>
2025-09-21 16:57:47 +0530

    Fix: profile tab is added!

--- frontend/src/utils/components/layout/Header.js
+++ frontend/src/utils/components/layout/Header.js
@@ -27,14 +27,14 @@ const Header = () => {
               Feed
             </Link>
 
-            {/*
+            {
             <Link 
               to={`/profile/${user.id}`} 
               className="px-3 py-2 text-gray-700 hover:text-blue-600 transition-colors"
             >
               Profile
             </Link>
-            */}
+            }
 
             <button
               onClick={handleLogout}
